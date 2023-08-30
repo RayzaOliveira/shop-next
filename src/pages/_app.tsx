15a354f3eb77f6e7c76e725d0c00e59fa418b21a
RayzaOliveira <rayza.ocr@gmail.com>
2023-08-30 18:49:25 -0300

    feat: Estrutura visual - Cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
-import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
+import { globalStyles } from "@/styles/global";
+
+import logoImg from "../assests/logo.svg";
+import { Container, Header } from "../styles/pages/app";
 
 //colocar fora do App, para não precisar recarregar quando sair da página
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }
