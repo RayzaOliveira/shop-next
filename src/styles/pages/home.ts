bf434c4b033dbd8469b7eec0a1b59f6814f5d2d9
RayzaOliveira <rayza.ocr@gmail.com>
2024-01-07 18:19:59 -0300

    feat: Estrutura visual - Criando carrossel

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -2,17 +2,15 @@ import { styled } from "..";
 
 export const HomeContainer = styled("main", {
   display: "flex",
-  gap: "3rem",
-  // width: "100%",
-  maxWidth: "calc(100vw-((100vw-1180px)/2))",
+  width: "100%",
+  maxWidth: "calc(100vw - ((100vw - 1180px) / 2))",
   marginLeft: "auto",
   minHeight: 656,
 });
 
-export const Product = styled("a", {
+export const Product = styled("div", {
   background: "linear-gradient(180deg, #1ea483 0%, #7465d4 100%)",
   borderRadius: 8,
-  padding: "0.25rem",
   cursor: "pointer",
   position: "relative",
   overflow: "hidden",
@@ -38,7 +36,7 @@ export const Product = styled("a", {
     alignItems: "center",
     justifyContent: "space-between",
 
-    backgroundColor: "rgba(0,0,0,0.6)",
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
 
     transform: "translateY(110%)",
     opacity: 0,
@@ -46,6 +44,7 @@ export const Product = styled("a", {
 
     strong: {
       fontSize: "$lg",
+      color: "$gray100",
     },
 
     span: {
