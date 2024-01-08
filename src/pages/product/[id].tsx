264e926cad9bd2d85dea3b5fb399070ac8081086
RayzaOliveira <rayza.ocr@gmail.com>
2024-01-07 23:38:13 -0300

    feat: Produto & Checkout - Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,28 @@
 import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/styles/pages/product";
 
 export default function Product() {
   // Usa o hook useRouter para obter os parâmetros da consulta da URL.
   const { query } = useRouter();
 
-  // Retorna um elemento h1 que mostra a palavra "Product:" seguida dos parâmetros da consulta convertidos em uma string JSON.
-  return <h1> Product: {JSON.stringify(query)}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,99</span>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim unde
+          temporibus non. Deleniti est, doloribus perspiciatis enim soluta,
+          praesentium dolor tempora provident minima maxime similique sapiente
+          cumque repudiandae explicabo voluptates.
+        </p>
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
